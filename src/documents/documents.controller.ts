0106bbe6e2be353d34da4a8b50669f39ad1f2ed3
Vitaly Pereslavtsev <dev@pereslavtsev.com>
2019-11-17 15:13:26 +0300

    + lint fixes

--- src/documents/documents.controller.ts
+++ src/documents/documents.controller.ts
@@ -1,7 +1,9 @@
 import { Controller } from '@nestjs/common';
 import { GrpcMethod } from '@nestjs/microservices';
 import { GetDocumentRequest } from './interfaces/requests/get-document-request.interface';
-import { ListDocumentSaveStories } from './interfaces/requests/list-document-save-stories.interface';
+import {
+  ListDocumentSaveStories,
+} from './interfaces/requests/list-document-save-stories.interface';
 import { DocumentsService } from './services/documents.service';
 import { SaveStoriesService } from './services/save-stories.service';
 
